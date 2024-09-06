4033bd0fc806b63cbaa0c7760f9e18cc1afa32a7
Ashwini Kumar Meena <118055656+itskirito@users.noreply.github.com>
2024-09-06 17:49:27 +0530

    Time: 87 ms (74.05%), Space: 75.6 MB (17.73%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,52 @@
+
+    //  unordered_map<int, int>mp;      // initializes a unordered map mp, for storing frequency of elements
+    //  for(auto &num : nums)
+    //  {
+    //      mp[num]++;
+    //  }   
+
+    //  int count = 0;
+    //  int max_Freq = INT_MIN;     // initializes two variables count and maxfreq, count kjeeps track
+    //  for(auto &m : mp)           // of total count of element with max freq and maxfreq holds the maximum frequency
+    //  {                           // found so far, goes through the map and updates if it founds greater freq than current.
+    //      if(m.second > max_Freq)
+    //      {
+    //          max_Freq = m.second;
+    //      }
+    //  }
+
+    //  for(auto &m : mp)
+    //  {                              
+    //      if(m.second == max_Freq)   // iterates through the map again to count the no of elements that have
+    //      {                          // maximum frequency, and increments the count by max_freq.
+    //          count += max_Freq;
+    //      }
+    //  }
+
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        unordered_map<int, int> mpp;
+
+        for(auto &num : nums)
+        {
+            mpp[num]++;
+        }    
+
+        for (auto &check : mpp)
+        {
+            if(check.second > 1)
+            {
+                return true;
+            }
+
+    
+            
+        }
+
+        return false;
+    }
+};
