201d031c8e46740248780611088b0cccb2043f64
Ashwini Kumar Meena <118055656+itskirito@users.noreply.github.com>
2024-09-24 15:38:26 +0530

    Time: 2 ms (76.43%), Space: 7.5 MB (86.86%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int josephus(int n, int k)
+    {
+        if(n == 1)
+        {
+            return 1;
+        }
+
+        return (josephus(n-1, k) + k-1)%n + 1;
+    }
+    int findTheWinner(int n, int k) 
+    {
+        return josephus(n, k);   
+    }
+};
