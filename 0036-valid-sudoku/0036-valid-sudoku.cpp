80e813a5246fbfa7be8d71173f862511811e1fb0
Ashwini Kumar Meena <118055656+itskirito@users.noreply.github.com>
2024-09-25 22:17:03 +0530

    Time: 31 ms (5.51%), Space: 26.6 MB (5.55%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) 
+    {
+        unordered_map<int, unordered_set<char>> rows;
+        unordered_map<int, unordered_set<char>> cols;
+        unordered_map<int, unordered_set<char>> boxes;
+
+        for(int r = 0; r < 9; r++)
+        {
+            for(int c = 0; c < 9; c++)
+            {
+                char cell = board[r][c];
+
+                if(cell == '.')
+                {
+                    continue;
+                }
+
+                if(rows[r].count(cell) ||
+                   cols[c].count(cell) ||
+                   boxes[(r/3)*3 + c/3].count(cell))
+                   {
+                        return false;
+                   }
+                
+                rows[r].insert(cell);
+                cols[c].insert(cell);
+                boxes[(r/3)*3 + c/3].insert(cell);
+            }
+        }
+
+        return true;
+    }
+};
